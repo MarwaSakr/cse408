6c105d579adb37ec71933b8764618537e2ee5a0f
Robert Peck <soldier.moth@gmail.com>
2010-09-29 18:52:52 -0700

    added option to save image

--- Menu.java
+++ Menu.java
@@ -6,6 +6,7 @@ public class Menu {
     public static final int SHIFT_HUE_KEY = 3;
     public static final int ADJUST_LIGHT_KEY = 4;
     public static final int REDUCE_COLOR_INSTANCES_KEY = 5;
+    public static final int SAVE_KEY = 6;
     public static final int DISPLAY_IMAGE_KEY = 7;
     public static final int PRINT_MENU_KEY = 8;
     public static final int EXIT_KEY = 9;
@@ -17,6 +18,7 @@ public class Menu {
         System.out.println(SHIFT_HUE_KEY + ": Shift the hue");
         System.out.println(ADJUST_LIGHT_KEY + ": Adjust light in image");
         System.out.println(REDUCE_COLOR_INSTANCES_KEY + ": Reduce Color Instances");
+        System.out.println(SAVE_KEY + ": Save image");
         System.out.println(DISPLAY_IMAGE_KEY + ": Display Image");
         System.out.println(PRINT_MENU_KEY + ": Print the Menu Again");
         System.out.println(EXIT_KEY + ": Exit");
