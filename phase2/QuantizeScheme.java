a391adc172d1ff24c0fd1bfa413608a4e41f847a
Robert Peck <soldier.moth@gmail.com>
2010-11-01 17:57:52 -0700

    updating fixes

--- phase2/QuantizeScheme.java
+++ phase2/QuantizeScheme.java
@@ -1,48 +1,62 @@
 import java.lang.Math;
+import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class QuantizeScheme {
     private int[] numBuckets;
-    private int[] minVal;
-    private int[] maxVal;
+    private static int[] minVal;
+    private static int[] maxVal;
     
 
     public static final int Y=0;
     public static final int U=1;
     public static final int V=2;
-    minVal[Y] = 0;
-    maxVal[Y] = 255;
-    minVal[U] = -128;
-    maxVal[U] = 128;
-    minVal[V] = -128;
-    maxVal[V] = 128;
+    
     
     public QuantizeScheme(int bucketY, int bucketU, int bucketV){
         numBuckets = new int[3];
-        numBuckets[Y] = numBucketsY;
-        numBuckets[U] = numBucketsU;
-        numBuckets[V] = numBucketsV;
+        numBuckets[Y] = bucketY;
+        numBuckets[U] = bucketU;
+        numBuckets[V] = bucketV;
+        minVal = new int[3];
+        maxVal = new int[3];
+        minVal[Y] = 0;
+        maxVal[Y] = 255;
+        minVal[U] = -128;
+        maxVal[U] = 128;
+        minVal[V] = -128;
+        maxVal[V] = 128;
     }
     public QuantizeScheme(){
         numBuckets = new int[3];
         numBuckets[Y] = 1;
         numBuckets[U] = 1;
         numBuckets[V] = 1;
+        minVal[Y] = 0;
+        maxVal[Y] = 255;
+        minVal[U] = -128;
+        maxVal[U] = 128;
+        minVal[V] = -128;
+        maxVal[V] = 128;
     }
     
     public int quantize(float num, int Type){
 	    int numBucket = numBuckets[Type];
-	    float stepSize = (abs(min)+abs(max))/numBucket;
+	    int min = minVal[Type];
+	    int max = maxVal[Type];
+	    float stepSize = (Math.abs(min)+Math.abs(max))/numBucket;
 	    if (numBuckets[Type] % 2 == 0){
-	        return (int) (ceiling(num)-stepSize/2);
+	        return (int) (ceiling(num,Type)-stepSize/2);
 	    } else {
-	        return (int) (floor(num-stepSize/2));
+	        return (int) (floor(num-stepSize/2,Type));
 	    }
     }
     
     private float ceiling(float num, int Type){
+        int numBucket = numBuckets[Type];
         int min = minVal[Type];
 	    int max = maxVal[Type];
-	    float stepSize = (abs(min)+abs(max))/numBucket;
+	    float stepSize = (Math.abs(min)+Math.abs(max))/numBucket;
         float ceil = min;
         while(ceil < num){
             ceil += stepSize;
@@ -50,9 +64,10 @@ public class QuantizeScheme {
         return ceil;
     }
     private float floor(float num, int Type){
+        int numBucket = numBuckets[Type];
         int min = minVal[Type];
 	    int max = maxVal[Type];
-	    float stepSize = (abs(min)+abs(max))/numBucket;
+	    float stepSize = (Math.abs(min)+Math.abs(max))/numBucket;
 	    float floor = max;
 	    while(floor > num){
 	        floor = floor-stepSize;
@@ -60,6 +75,7 @@ public class QuantizeScheme {
         return floor;
     }
     public static int prompt(int Type){
+        Scanner scan = new Scanner(System.in);
         String typeName = "Y";
         if(Type == 1) typeName = "U";
         if(Type == 2) typeName = "V";
