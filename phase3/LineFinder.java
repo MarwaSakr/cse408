0f857df0c9f4f954984915cda4c49d9f5025c2f4
Robert Peck <soldier.moth@gmail.com>
2010-11-28 21:00:38 -0700

    adding test pictures and basic setup for task 4

---
+++ phase3/LineFinder.java
@@ -0,0 +1,7 @@
+public class LineFinder {
+    public LineFeature[] find(GridLayout grid, TextureFeature[] texture, MagickImage image){
+        
+        
+        System.out.println("(X:"+"x"+",Y:"+"y"+"Theta:"+"theta"+"R:"+"r"+")";
+    }
+}
