51425e2ba04fe0bffc1785ed99185cce82f92a25
Robert Peck <soldier.moth@gmail.com>
2010-11-28 23:16:50 -0700

    Updating skeleton code

--- phase3/LineFinder.java
+++ phase3/LineFinder.java
@@ -1,7 +1,10 @@
 public class LineFinder {
-    public LineFeature[] find(GridLayout grid, TextureFeature[] texture, MagickImage image){
-        
+    public static LineFeature[] find(GridLayout grid, TextureFeature[] texture, MagickImage image){
+        //For Each grid locale find params of 5 major lines using texture Feature as input 
         
         System.out.println("(X:"+"x"+",Y:"+"y"+"Theta:"+"theta"+"R:"+"r"+")";
+    }
+    private static LineFeature[] subFind(MagickImage subImage, TextureFeature[] texture){
+        //Create 
     }
 }
