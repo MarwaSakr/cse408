4c1f4538c847a6b6635a26a27ccddd277b0e6647
Robert Peck <soldier.moth@gmail.com>
2010-12-04 15:05:06 -0700

    updating linfinder/linefeat

--- phase3/LineFeature.java
+++ phase3/LineFeature.java
@@ -1,8 +1,10 @@
 public class LineFeature {
+    String fileName;
     double theta;
     double r;
 
-    public LineFeature(double theta, double r){
+    public LineFeature(double theta, double r, String filename){
+        this.fileName = filename;
         this.theta = theta;
         this.r = r;
     }
